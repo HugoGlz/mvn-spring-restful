e1895f6ed8778182ee43e3fc78e9c0fd6e8f9665
Hugo  Gonzalez <jugo@Mac-mini.local>
2019-12-28 12:33:51 -0600

    initial version

---
+++ src/main/java/jugo/GreetingController.java
@@ -0,0 +1,24 @@
+package jugo;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private final static String TEMPLATE = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping("/greeting")
+	public Greeting greeting(
+			@RequestParam( value="name", defaultValue="World") String name
+	) {
+		Greeting greetingResponse = new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
+		return greetingResponse;
+	}
+	
+}
